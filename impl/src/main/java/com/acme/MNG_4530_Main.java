23b65c553963eacf85da4c5a1493579dfa19afe6
strohmattenverleger <strohmattenverleger@users.noreply.github.com>
2021-07-25 12:04:50 +0200

    MNG-4530 Add alias feature for moved/renamed dependencies

---
+++ impl/src/main/java/com/acme/MNG_4530_Main.java
@@ -0,0 +1,13 @@
+package com.acme;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class MNG_4530_Main {
+
+    private static final Log LOG = LogFactory.getLog(MNG_4530_Main.class);
+
+    public static void main(String[] args) {
+        LOG.info("Running " + MNG_4530_Main.class.getSimpleName() + "#main(String[])");
+    }
+}
